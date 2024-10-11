0eb54309614a81247e94504cb1f82c6d40dbf442
alanovDoolotkeldi <doolotkeldialanov12.com>
2024-10-11 15:25:54 +0600

    add garand

--- src/components/reklam/Reklam.jsx
+++ src/components/reklam/Reklam.jsx
@@ -1,62 +1,100 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Reklam() {
-  return (
-    <div className='wrapper'>
-        <div className="wrapper-content">
-            <div className="container">
-                <div className="wrapper-text">
+  const sliderRef = useRef(null);
+  const [marginLeft, setMarginLeft] = useState(0); // Начальное значение 20px
+  const [number, setNumber] = useState(1)
 
-               <span>Новинка</span>
-               <div className="wrapper-catalog">
-                <h1>ЭЛЕКТРОСОМОКАТЫ KUGOO KIRIN ОТ ОФИЦИАЛЬНОГО ДИЛЕРА</h1>
-                <p>с бесплатной доставкой по РФ от 1 дня</p>
-                <button>Перейти в католог</button>
-
-               </div>
-                </div>
-                <div className="wrapper-center">
-                    <div className="reklam-test-info">
-
-                    <div className="reklam-test">
-                        <img src="/public/assets/svg/lightning.svg" alt="" />
-                        Тест-драйв и обучение
-
-                    </div>
-                    <button>Бесплатно</button>
-                    <img className='round-1' src="/public/assets/svg/round.svg" alt="" />
-                    <img className='round-2' src="/public/assets/svg/round.svg" alt="" />
-                    <img src="/public/assets/svg/line.svg" alt="" className="line" />
-                    </div>
-
-
-                </div>
-
-            </div>
-        </div>
-        <div className="wrapper-navigation">
-            <div className="left">
-                <img src="/public/assets/svg/lest.svg" alt="" />
-            </div>
-            <div className="wrapper-main">
-                <p>1</p>
-                <div className="change">
-                    <div className="change-item">
-
-                    </div>
-                </div>
-                <p>5</p>
-
-            </div>
-            <div className="right">
-                <img src="/public/assets/svg/right.svg" alt="" />
-            </div>
-
-        </div>
-        
+  const settings = {
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    beforeChange: (current, next) => {
+      // Сброс margin-left на 20px при переходе на первый слайд
+      if (next === 0) {
+        setMarginLeft(20);
+        setNumber(5)
+      }
       
+    },
+  };
+
+  const handlePrev = () => {
+    sliderRef.current.slickPrev();
+    setMarginLeft((prev) => Math.max(prev - 5, 0));
+    if(number>1){
+
+      setNumber(number-1)
+    }
+     // Уменьшаем margin, но не меньше 0
+     
+     
+  };
+
+  const handleNext = () => {
+    const maxMargin = 19; // Максимальное значение смещения
+    sliderRef.current.slickNext();
+    setMarginLeft((prev) => Math.min(prev + 5, maxMargin)); // Увеличиваем margin до maxMargin
+    if(number<5){
+
+      setNumber(number+1)
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <Slider ref={sliderRef} {...settings}>
+        {[1, 2, 3, 4].map((_, index) => (
+          <div className="wrapper-content" key={index}>
+            <div className="container">
+              <div className="wrapper-text">
+                <span>Новинка</span>
+                <div className="wrapper-catalog">
+                  <h1>ЭЛЕКТРОСОМОКАТЫ KUGOO KIRIN ОТ ОФИЦИАЛЬНОГО ДИЛЕРА</h1>
+                  <p>с бесплатной доставкой по РФ от 1 дня</p>
+                  <button>Перейти в каталог</button>
+                </div>
+              </div>
+              <div className="wrapper-center">
+                <div className="reklam-test-info">
+                  <div className="reklam-test">
+                    <img src="/assets/svg/lightning.svg" alt="Lightning" />
+                    Тест-драйв и обучение
+                  </div>
+                  <button>Бесплатно</button>
+                  <img className="round-1" src="/assets/svg/round.svg" alt="Round" />
+                  <img className="round-2" src="/assets/svg/round.svg" alt="Round" />
+                  <img src="/assets/svg/line.svg" alt="Line" className="line" />
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Slider>
+
+      <div className="wrapper-navigation">
+        <div className="left" onClick={handlePrev}>
+          <img src="/assets/svg/lest.svg" alt="Left" />
+        </div>
+        <div className="wrapper-main">
+          <p>{number}</p>
+          <div className="change">
+            <div
+              className="change-item"
+              style={{ marginLeft: `${marginLeft}px` }}
+            ></div>
+          </div>
+          <p>5</p>
+        </div>
+        <div className="right" onClick={handleNext}>
+          <img src="/assets/svg/right.svg" alt="Right" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Reklam
+export default Reklam;
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Reklam from '../../components/reklam/Reklam'
+import Garant from '../../components/garant/Garant'
+import Filter from '../../components/filter/Filter'
+import Carts from '../../components/cards/Carts'
+import Model from '../../components/model/Model'
+import Dolivery from '../../components/dolivery/Dolivery'
 
 function HomePage() {
   return (
     <div>
        <Reklam/>
+       <Garant/>
+       <Filter/>
+       <Carts/>
+       <Model/>
+       <Dolivery/>
+       <div style={{height:"100px"}}></div>
       
     </div>
   )
